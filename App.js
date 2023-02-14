5912ee5064d37bb6a65bf8cb3ded34e058879953
SaulMoreyra <armhacam21@gmail.com>
2023-02-14 17:16:46 -0600

    feat: add icons

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import CoffetiButton from "./CoffetiButton";
+import FloattingButton from "./FloattingButton";
+import RotationButton from "./RotationButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RotationButton style={{ left: 20, bottom: 100 }} />
+      <FloattingButton style={{ left: 80, bottom: 100 }} />
+      <CoffetiButton style={{ left: 140, bottom: 100 }} />
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
